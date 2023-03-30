f5ce4fc051aafa9eed3ef40299db49409b69b236
Dazzy Ding <h@dazzyd.org>
2023-03-30 17:25:33 +0800

    feat: remove noreferrer for outsite links

--- src/components/LinkButtons.js
+++ src/components/LinkButtons.js
@@ -9,14 +9,14 @@ export default function LinkButtons({ codex, floated }) {
   return (
     <Button.Group floated={floated} onClick={handleClickButton}>
       <Button icon='home'
-        as='a' target='_blank' rel="noreferrer"
+        as='a' target='_blank'
         href={`https://playorna.com${codex.path}`} />
       <Button icon='bookmark' disabled={codex.ornaguide_id == null}
-        as='a' target='_blank' rel="noreferrer"
+        as='a' target='_blank'
         href={`https://orna.guide/${codex.ornaguide_category}s?show=${codex.ornaguide_id}`}
       />
       <Button icon='calculator' disabled={codex.ornaguide_category !== "item"}
-        as='a' target='_blank' rel="noreferrer"
+        as='a' target='_blank'
         href={`https://orna.guide/assess?item=${codex.ornaguide_id}`}
       />
     </Button.Group>
