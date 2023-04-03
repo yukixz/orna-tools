4421b89025a3d9b62c3e7f055daa1c2c58c30170
Dazzy Ding <h@dazzyd.org>
2023-04-03 16:23:53 +0800

    feat: use browser router instead of hash one

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { RouterProvider, createHashRouter } from 'react-router-dom'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import CodexPage from './pages/CodexPage'
 import SearchPage from './pages/SearchPage'
 import { StoreProvider } from './context/StoreContext'
 import 'semantic-ui-css/semantic.min.css'
 import './App.css'
 
-const router = createHashRouter([
+const router = createBrowserRouter([
   {
     path: '/',
     element: <SearchPage />
